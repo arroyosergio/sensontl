84e1d35aa17b36cac66e615339b11e26be47631d
Hugh <kakistos741@gmail.com>
2018-04-15 17:37:48 -0500

    Informacion disponible, Guia e Informacion

--- views/guia/index.php
+++ views/guia/index.php
@@ -4,7 +4,30 @@
             </div>
             <div>
                 
-                <p>Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. At elementum eu facilisis sed odio morbi quis commodo odio. Non consectetur a erat nam at lectus. Duis at consectetur lorem donec. Non curabitur gravida arcu ac. Sed lectus vestibulum mattis ullamcorper velit. Non blandit massa enim nec dui nunc mattis.</p>
+                 <!-- Frame-container contiene los elementos para desplegar en el marco  -->
+			<div class="frame-container">
+					<div class="frame container-fluid">
+						<div class="frame-message row">
+                            <div class="col-lg-3 col-sm-3">
+                                <br/>
+						    	<a href="<?php echo URL.'public/images/sala_de_espera.png;' ?>" title="" data-fluidbox>
+                                    <img class="img-fluid img-responsive image_center" src="<?php echo URL.'public/images/sala_de_espera.png'; ?>" title="" alt="" />
+                                </a>	
+						    </div>
+						    <div class="col-lg-9 col-sm-12">
+                                <br/>
+                                <br/>
+                                <h3><p class="text-center" ><font color="#19194d">Información disponible próximamente.</font></p></h3>
+                                <br/>
+                                <br/>
+                               <!--<p>Emitido por: Autor</p>-->
+						    </div>
+						</div>
+					</div>
+            </div>
+            <br/>
+                
+                <!--<p>Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. At elementum eu facilisis sed odio morbi quis commodo odio. Non consectetur a erat nam at lectus. Duis at consectetur lorem donec. Non curabitur gravida arcu ac. Sed lectus vestibulum mattis ullamcorper velit. Non blandit massa enim nec dui nunc mattis.</p>
                 
                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 
@@ -16,6 +39,6 @@
                     <h3 class="text-center">Plantillas de trabajo</h3>				  
                 </div>
 				<p>Para descargar la plantilla de trabajo haz click en la imagen <a href=""><i  class="fa  fa-file-word-o fa-2x" aria-hidden="true" ></i></a></p>
-                <p>Para descargar la plantilla de trabajo haz click en la imagen <a href=""><i  class="fa  fa-file-word-o fa-2x" aria-hidden="true" ></i></a></p>
+                <p>Para descargar la plantilla de trabajo haz click en la imagen <a href=""><i  class="fa  fa-file-word-o fa-2x" aria-hidden="true" ></i></a></p> -->
             </div>
         </div>
--- views/informacion/index.php
+++ views/informacion/index.php
@@ -2,7 +2,30 @@
             <div class="container_title feature-head wow fadeInDown animated"  data-wow-animation-name="fadeInDown" style="visibility: visible; animation-name: fadeInDown;" >
                     <h2 class="text-center">Información para los Asistentes</h2>				  
             </div>
-            <div>
+            
+             <!-- Frame-container contiene los elementos para desplegar en el marco  -->
+			<div class="frame-container">
+					<div class="frame container-fluid">
+						<div class="frame-message row">
+                            <div class="col-lg-3 col-sm-3">
+                                <br/>
+						    	<a href="<?php echo URL.'public/images/sala_de_espera.png;' ?>" title="" data-fluidbox>
+                                    <img class="img-fluid img-responsive image_center" src="<?php echo URL.'public/images/sala_de_espera.png'; ?>" title="" alt="" />
+                                </a>	
+						    </div>
+						    <div class="col-lg-9 col-sm-12">
+                                <br/>
+                                <br/>
+                                <h3><p class="text-center" ><font color="#19194d">Información disponible próximamente.</font></p></h3>
+                                <br/>
+                                <br/>
+                               <!--<p>Emitido por: Autor</p>-->
+						    </div>
+						</div>
+					</div>
+            </div>
+            <br/>
+            <!--<div>
                 
                 <p>Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. At elementum eu facilisis sed odio morbi quis commodo odio. Non consectetur a erat nam at lectus. Duis at consectetur lorem donec. Non curabitur gravida arcu ac. Sed lectus vestibulum mattis ullamcorper velit. Non blandit massa enim nec dui nunc mattis.</p>
                 
@@ -10,7 +33,7 @@
                 
                 <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur, adipiscin] velit, sed quia non numquam do eius modi tempora incididunt, ut labore et dolore magnam aliquam quaerat voluptatem.</p>
                 
-                <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur.</p>
+                <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur.</p> -->
                 
             </div>
         </div>
