69ed75bdd9f016970e9e5c4ed6f2452901038118
Hugh <kakistos741@gmail.com>
2018-04-16 15:36:12 -0500

    Registro pago public con subir archivo

--- config/paths.php
+++ config/paths.php
@@ -2,7 +2,7 @@
 
 define('URL', "http://localhost/sensontl/"); //"http://localhost/cica2016/" "http://higo-software.com/controlCongresos/"
 define('DOCS', $_SERVER['DOCUMENT_ROOT'] .'/sensontl/docs/');///congreso/docs/  /cica2016/docs/
-define('DOCSDEPOSITOSPUBLICOS', $_SERVER['DOCUMENT_ROOT'] .'/sensontl/docs/dep_publico');
+define('DOCSDEPOSITOSPUBLICOS', $_SERVER['DOCUMENT_ROOT'] .'/sensontl/docs/dep_publico/');
 define('MENULATERAL',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menu.html');
 define('MENUADMIN',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menuadmin.html');
 define('MENUEDITOR',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menueditor.html');
